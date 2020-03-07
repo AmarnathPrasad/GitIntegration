2533e5824755a169212121f6b2310c9be68e6e89
amar <amarnathsonu@gmail.com>
2020-03-07 20:32:44 +0530

    push3

--- app/src/main/java/mvvm_new/amar/git/MainActivity.java
+++ app/src/main/java/mvvm_new/amar/git/MainActivity.java
@@ -2,6 +2,7 @@ package mvvm_new.amar.git;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
 
     //push2
     Button btn_push;
+
+    //push3
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
